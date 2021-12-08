8c40f04459e951e4b34f069b0b9d24fea9aec085
Orlando Caraballo <orlandocaraballo@gmail.com>
2021-12-08 17:38:51 -0500

    Added notes

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -2,11 +2,14 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const axios = require('axios');
 
-const Product = db.define('product', {
+const Product = db.define('product', 
+  // o: change name to "name"
   productName: {
     type: Sequelize.STRING,
     allowNull: false,
   },
+  // o: store pennies as an integer
+  //  divide by 100 on frontend
   price: {
     type: Sequelize.DECIMAL(5, 2),
     allowNull: false,
@@ -17,6 +20,7 @@ const Product = db.define('product', {
     // if image does not pop up (or is not found)
     // defaultValue: default-product-image.jpg
   },
+  // o: work on this later, remove now
   rating: {
     type: Sequelize.INTEGER,
     validate: {
@@ -33,11 +37,13 @@ const Product = db.define('product', {
     },
     allowNull: false,
   },
+  // o: work on this later, remove now
   category: {
     type: Sequelize.ENUM({
       values: ['Cleansers', 'Toners', 'Exfoliators', 'Moisturizers'], //we'll name these categories Wednesday morning
     }),
   },
+  // o: modify to string
   SKU: {
     type: Sequelize.INTEGER,
   },
