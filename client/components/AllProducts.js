107d1787b34db9f0736ee87f912df0bccdfbd487
Joel Bascombe <joelbascombe13@gmail.com>
2021-12-09 12:37:27 -0500

    All Product Component in progress

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -3,7 +3,21 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default class AllProducts extends Component {
+  // componentDidMount() {
+  //   try {
+  //     this.props.//Allproject();
+  //   } catch (error) {
+  //     console.log("error inside component getProjects ", error);
+  //   }
+  // }
   render() {
-    return <div></div>;
+    return (
+      <>
+        <div>
+          <Link></Link>
+        </div>
+        <h2>Welcome to Our Products Page!</h2>
+      </>
+    );
   }
 }
