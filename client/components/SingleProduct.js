4a88c0ef187974207214e8f9ea6d0b1d2ddee980
Malcolm Mack <malcolmtechlearning@gmail.com>
2021-12-09 15:29:43 -0500

    Built out SingleProduct React Component

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchProduct } from '../store/singleProduct';
+
+export class SingleProduct extends Component {
+  componentDidMount() {
+    this.props.fetchProduct(this.props.match.params.productId);
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <div>
+            <h1>{this.props.storedProduct.name}</h1>
+            <img src={this.props.storedProduct.imgUrl} />
+            <h2>{this.props.storedProduct.price / 100}</h2>
+          </div>
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapState = (state) => {
+  return state.singleProduct;
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    fetchProduct: (singleProduct) => dispatch(fetchProduct(singleProduct)),
+  };
+};
+
+export default connect(mapState, mapDispatch)(SingleProduct);
