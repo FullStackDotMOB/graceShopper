fd8aea5c85c12df21cf11e548906009b60ff4ba5
Malcolm Mack <malcolmtechlearning@gmail.com>
2021-12-09 16:17:22 -0500

    singleProductReducer added to combinesReducer

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { fetchProduct } from '../store/singleProduct';
 
-export class SingleProduct extends Component {
+export class SingleProduct extends React.Component {
   componentDidMount() {
     this.props.fetchProduct(this.props.match.params.productId);
   }
 
   render() {
+    console.log(this.props.product);
     return (
-      <div>
-        <ul>
-          <div>
-            <h1>{this.props.storedProduct.name}</h1>
-            <img src={this.props.storedProduct.imgUrl} />
-            <h2>{this.props.storedProduct.price / 100}</h2>
-          </div>
-        </ul>
-      </div>
+      <h1>This is Single Product Page</h1>
+      //   <div>
+      //     <ul>
+      //       <div>
+      //         <h1>{this.props.storedProduct.name}</h1>
+      //         <img src={this.props.storedProduct.imgUrl} />
+      //         <h2>{this.props.storedProduct.price / 100}</h2>
+      //       </div>
+      //     </ul>
+      //   </div>
     );
   }
 }
 
-const mapState = (state) => {
-  return state.singleProduct;
-};
+// const mapState = (state) => {
+//   return state.singleProduct;
+// };
 
 const mapDispatch = (dispatch) => {
   return {
@@ -32,4 +34,4 @@ const mapDispatch = (dispatch) => {
   };
 };
 
-export default connect(mapState, mapDispatch)(SingleProduct);
+export default connect(null, mapDispatch)(SingleProduct);
