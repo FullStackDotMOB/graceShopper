8edbc27deddeeddc724f374d6ed803c6c836c934
Malcolm Mack <malcolmtechlearning@gmail.com>
2021-12-09 14:44:20 -0500

    built out redux store for singleProduct

---
+++ client/store/singleProduct.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const RETRIEVE_SINGLE_PRODUCT = 'RETRIEVE_SINGLE_PRODUCT';
+
+export const setProduct = (product) => {
+  return { type: RETRIEVE_SINGLE_PRODUCT, product };
+};
+
+export const fetchProduct = (productNumber) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/products/${productNumber}`);
+      dispatch(setProduct(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+const initialState = {
+  storedProduct: [],
+};
+
+export default function singleProductReducer(state = initialState, action) {
+  switch (action.type) {
+    case RETRIEVE_SINGLE_PRODUCT:
+      return { ...state, storedProduct: action.robot };
+    default:
+      return state;
+  }
+}
