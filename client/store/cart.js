be97c8f0410d5240234d34e296613e11c4f29431
Malcolm Mack <malcolmtechlearning@gmail.com>
2021-12-16 18:33:10 -0500

    cart redux store set up

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-// const CART_LOAD = 'CART_LOAD';
 const VIEW_CART = 'VIEW_CART';
-
-// const cartLoad = () => ({
-//   type: CART_LOAD,
-// });
+const ADD_TO_CART = 'ADD_TO_CART';
 
 const viewCart = (cart) => ({ type: VIEW_CART, cart });
+const addToCart = (cart) => ({ type: ADD_TO_CART, cart });
 
 export const fetchCart = (userId) => async (dispatch) => {
   try {
@@ -21,14 +18,24 @@ export const fetchCart = (userId) => async (dispatch) => {
   }
 };
 
+export const addProductToCart = (userId, productId) => async (dispatch) => {
+  try {
+    const { data } = await axios.post(`/api/users/${userId}/cart/${productId}`);
+    console.log('Add to Product Data:', data);
+    dispatch(addToCart(data));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const initialState = [];
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
-    // case CART_LOAD:
-    //   return { ...state, loading: true };
     case VIEW_CART:
       // Orlando: why is this adding new products, when you have a cart key
+      return action.cart;
+    case ADD_TO_CART:
       return action.cart;
     default:
       return state;
